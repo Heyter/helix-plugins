a31dee213a2556edefa79a6166832bd1b837fe04
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2020-05-11 21:12:35 +0300

    Add files via upload

--- durability/items/sh_remnabor_armor.lua
+++ durability/items/sh_remnabor_armor.lua
@@ -1,4 +1,4 @@
 ITEM.name = "Набор для починки брони"
 ITEM.category = "misc"
-ITEM.desc = "Короб, в котором находится все необходимое для починки брони."
+ITEM.description = "Короб, в котором находится все необходимое для починки брони."
 ITEM.model = "models/toussaint_box1.mdl"
--- durability/items/sh_remnabor_weapon.lua
+++ durability/items/sh_remnabor_weapon.lua
@@ -1,4 +1,4 @@
 ITEM.name = "Набор для починки оружия"
 ITEM.category = "misc"
-ITEM.desc = "Короб, в котором находится все необходимое для починки оружия."
+ITEM.description = "Короб, в котором находится все необходимое для починки оружия."
 ITEM.model = "models/toussaint_box1.mdl"
