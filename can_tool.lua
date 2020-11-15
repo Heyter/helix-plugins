822a165f162183d8bf279fd11c0874176e20520a
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2020-11-16 01:36:23 +0300

    Add files via upload

---
+++ can_tool.lua
@@ -0,0 +1,30 @@
+PLUGIN.name = "CanTool"
+PLUGIN.author = "Hikka"
+PLUGIN.description = "Overwrite CanTool method"
+
+local NO_DUPLICATE_ENTS = {}
+NO_DUPLICATE_ENTS['ix_money'] = true
+NO_DUPLICATE_ENTS['ix_item'] = true
+NO_DUPLICATE_ENTS['ix_shipment'] = true
+
+do
+	local TOOL_DANGEROUS = {}
+	TOOL_DANGEROUS["dynamite"] = true
+	TOOL_DANGEROUS["duplicator"] = true
+	
+	function GAMEMODE:CanTool(client, trace, tool_name)
+		if (tool_name == "duplicator" and IsValid(trace.Entity) and (trace.Entity.NoDuplicate or NO_DUPLICATE_ENTS[trace.Entity:GetClass()])) then
+			return false
+		end
+		
+		if (client:IsAdmin()) then
+			return true
+		end
+
+		if (TOOL_DANGEROUS[tool_name]) then
+			return false
+		end
+		
+		return self.BaseClass:CanTool(client, trace, tool_name)
+	end
+end
